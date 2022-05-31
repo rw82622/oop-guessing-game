f20446c38975d28e256ccb24a09834e111e000e4
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-31 15:17:16 -0500

    python solution for guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,24 @@
 class GuessingGame():
+    def __init__(self, answer):
+        self.answer = answer
+        self.is_solved = False
+        
+        
+    def guess(self, user_guess):
+        self.user_guess = user_guess
+        if user_guess > self.answer:
+            print("too high")
+        elif user_guess < self.answer:
+            print("too low")
+        elif user_guess == self.answer:
+            self.is_solved = True
+            print("correct")
+        
+    def solved(self):
+        print(self.is_solved)
+        
+        
+game = GuessingGame(11)
+game.guess(11)
+game.solved()
+        
