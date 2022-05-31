cf0c6169ca265eee3b63834b34ddd8e38603c5cd
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-31 17:53:14 -0500

    fix bug that keeps the program running even when the user wants to stop

--- guessing_game_random.py
+++ guessing_game_random.py
@@ -6,12 +6,13 @@ class GuessingGameRandom():
         self.continuePlaying = True
         self.is_solved = False
         self.count = 0
+        self.user_guess = ""
         
     def guess(self):
         self.response = []
-        self.user_guess = int(input("Enter your guess\n"))
         
         while self.continuePlaying:
+            self.user_guess = int(input("Enter your guess\n"))
             self.count += 1
             if self.user_guess > self.answer:
                 self.response = ["Unfortunately", "too high"]
@@ -22,19 +23,15 @@ class GuessingGameRandom():
                 self.response = ["Congratulations", "correct"]
 
             print(f"{self.response[0]} your answer was {self.response[1]}")
+            
             if self.is_solved:
-                player_commitment = input(f'You guessed the correct answer in {self.count} tries.\nWould you like to play again? Y/N\n')
-                if player_commitment == "Y" or "y":
-                    self.answer = random.randint(1, 25)
+                player_commitment = input(f'You guessed the correct answer in {self.count} tries.\nWould you like to play again? Y/N\n').upper()
+                if player_commitment == "Y":
+                    self.answer = random.randint(1, 3)
                     self.is_solved = False
                     self.count = 0
-                    # self.guess()
-                elif player_commitment == "N" or "n":
+                else: 
                     self.continuePlaying = False
-
-                print("Didn't work")
-            if self.continuePlaying: 
-                self.guess()
                 
                 
 game = GuessingGameRandom()
